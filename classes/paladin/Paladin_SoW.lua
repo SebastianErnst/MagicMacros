260a936ac83ca604661d21e80884b48d73206d74
Basti <ernst.sebastian1991@mail.com>
2025-11-25 00:05:24 +0100

    feat(paladin): Only judge wisdom if it's not already on the target otherwise it does nothing

--- classes/paladin/Paladin_SoW.lua
+++ classes/paladin/Paladin_SoW.lua
@@ -1,9 +1,12 @@
 function Paladin_SoW()
     local textureName = "Spell_Holy_RighteousnessAura"
     local isBuffFound = buffs_findBuffByTextureName(textureName)
+    local isAlreadyDebuffed = buffs_findDebuffByTextureName(textureName)
 
-    if isBuffFound then
-        CastSpellByName("Judgement")
+    if isBuffFound and not isAlreadyDebuffed then
+        if isAlreadyDebuffed then
+            CastSpellByName("Judgement")
+        end
     else
         CastSpellByName("Seal of Wisdom")
     end
